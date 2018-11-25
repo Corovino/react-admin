bc147ba9dd79183094f1ca25c343642fd9e759d3
Hebert Romero <r_sastoque@hotmail.com>
2018-11-24 19:46:42 -0500

    ajustes  filytod bi sort

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { setTextFilter , sortByAmount, sortByDate } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import logo from './logo.svg';
 
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('Water'));
+store.dispatch(addExpense({ description: 'Water bill', amount:400 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt:100 }));
+store.dispatch(addExpense({ description: 'Rent', amount:109500 }));
+//store.dispatch( sortByAmount() );
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
--- src/Components/EspenseListFilter.js
+++ src/Components/EspenseListFilter.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from '../actions/filters';
+import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
 
 
 const onChangeHandler = (e , props) => {
     const inputValue = e.target.value;
-    console.log(inputValue);
     props.dispatch(setTextFilter(inputValue));
+}
+
+const handleDataBy = (e, props) => {
+    console.log(e.target.value);
+    switch(e.target.value){
+       case"date":
+          props.dispatch(sortByAmount());
+       break;
+       case"amount":
+          props.dispatch(sortByDate());
+       break;
+
+    }
 }
 
 const ExpenseListFilters = (props) => (
   <div>
     <input type="text" value={props.filters.text} onChange={ e => onChangeHandler(e, props) } />
+    <select
+        values={props.filters.sortBy} 
+        onChange={ e => handleDataBy(e, props) }> 
+       <option  value="date">Date</option>
+       <option  value="amount">Amount</option>
+    </select>
   </div>
 );
 
